39c18498134ab8a418a0c2684ae9211e8c058159
bhavana-csg <bhavana.j@global-csg.com>
2024-03-19 14:36:06 +0530

    Including Userstory1

---
+++ printoutputandvariables.js
@@ -0,0 +1,60 @@
+//1.PRINTING OUTPUT
+function printoutput()
+{
+    console.log("Hi Bhavana ,A very good morning!!");
+}
+let output1=printoutput()
+console.log(output1);
+
+//2.ADDITION OF TWO NUMBERS
+
+function addnumbers(a,b)
+{
+var res=a+b;
+console.log(res);
+}
+
+let output2=addnumbers(2,5);
+console.log(output2);
+
+//3. UNDERSTANDING LET IN JS.
+
+function letvariable()
+{
+    let a = "bhavana";
+    let b = 10;
+    let c = 12;
+    let d = b + c;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+}
+let output3=letvariable();
+console.log(output3);
+
+// UNDERSTANDING CONST 
+
+function constdemo()
+{
+
+const a = "Hello learners"
+console.log(a);
+
+const b = 400;
+console.log(b);
+
+const c = "12";
+console.log(c);
+
+}
+let output4=constdemo();
+console.log(output4);
+
+
+
+
+
+
+
