08020302b5ae9f60e250279d6e2c71fa4c31ef7d
Narasinha Tippannavar <narasiti@creative.local>
2024-03-20 10:36:37 +0530

    US 2

--- functions.js
+++ functions.js
@@ -21,3 +21,47 @@ function sum(a,b){
     console.log(sum)
 }
 
+sum(3,4)
+
+const arrowMul = (a,b) => {
+    console.log(a*b)
+}
+
+arrowMul(3,4)
+
+
+//example
+function myFunc(theArr) {
+    theArr[0] = 30;
+  }
+  
+  const arr = [45];
+  
+  console.log(arr[0]); // 45
+  myFunc(arr);
+  console.log(arr[0]); // 30
+
+  //example
+  function map(f, a) {
+    const result = new Array(a.length);
+    for (let i = 0; i < a.length; i++) {
+      result[i] = f(a[i]);
+    }
+    return result;
+  }
+  
+  const cube = function (x) {
+    return x * x * x;
+  };
+  
+  const numbers = [0, 1, 2, 5, 10];
+  console.log(map(cube, numbers)); // [0, 1, 8, 125, 1000]
+
+
+  //Eample
+  array = [1,2,3,4,5];
+
+  arr.forEach(function printVal(val) {
+    console.log(val);
+  })
+
